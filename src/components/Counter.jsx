8fa7e0524898d6e760841b39fe83bd6f74718bfe
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-09-09 22:36:18 +0600

    use useSelector for reflex or update value then change the value frontend and use dispatch

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,18 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../redux/features/counter/counterSlice";
 
 function Counter() {
     const {count} = useSelector(state =>state.counter);
+     const dispatch=useDispatch();
     console.log(count);
   return (
 
     <div>
-        <button>Increment</button>
+        <button onClick={()=>dispatch(increment())}>Increment</button>
         <div>
-            <h1>0</h1>
+            <h1>{count}</h1>
         </div>
-        <button>Decrement</button>
+        <button onClick={()=>dispatch(decrement())}>Decrement</button>
     </div>
   )
 }
