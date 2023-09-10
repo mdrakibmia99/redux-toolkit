e90aa195d2bff47398ef30f265bd61e06a0e34c7
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-09-10 11:56:30 +0600

    create a logger which show current state ,dispatch action,update state

--- src/redux/middleWares/logger.js
+++ src/redux/middleWares/logger.js
@@ -1,6 +1,9 @@
 const logger=(store)=>(next)=>(action)=>{
 
     const currentState=store.getState();
-    console.log(currentState)
+    console.log("Current State",currentState)
+    console.log("Dispatch Action",action)
+    next(action);
+    console.log(store.getState())
 };
 export default logger;
