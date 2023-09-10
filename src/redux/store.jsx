5f932fb394418cebdd784c7bbb5bb71fc214fa5a
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-09-10 11:49:21 +0600

    create a logger in rdeux file like middleware=>logger.js and connect with store

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,12 +1,13 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
+import logger from './middleWares/logger'
 const store = configureStore({
     reducer: {
 
         counter: counterReducer
     },
     // middleware: (getDefaultMiddleware) =>getDefaultMiddleware().concat(loggerMiddleware),
-    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat()
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(logger)
     
 });
 export default store;
